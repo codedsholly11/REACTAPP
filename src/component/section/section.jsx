df50244baa511bf245a38bb2ee574266cee6bf45
codedsholly11 <codedsholly@gmail.com>
2024-07-15 18:35:43 +0100

    latest commit

--- src/component/section/section.jsx
+++ src/component/section/section.jsx
@@ -7,7 +7,7 @@ const section = () => {
       <div className=''>
         <div  className='boost bg-no-repeat w-[100%] h-[25vh] flex flex-col items-center justify-center gap-[20px] '>
           <h2 className='text-white text-2xl font-semibold md:text-4xl md:font-semibold  '>Boost your links today</h2>
-          <button className='py-[10px] px-[20px] item-center rounded-full w-[140px] h-[35px] text-white hover:bg-cyan-700 bg-cyan-400 '>Get Started</button>
+          <button className='py-[10px] px-[20px] rounded-full w-[140px] text-white hover:bg-cyan-700 bg-cyan-400 '>Get Started</button>
         </div>
 
       </div>
